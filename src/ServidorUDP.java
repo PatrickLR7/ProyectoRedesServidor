80bb1011374b629d312b179dc50900b421042943
PatrickLR7 <patrickrx7@gmail.com>
2018-06-24 14:27:33 -0600

    Funcionalidad básica de servidor implementada (funciona localmente).

--- src/ServidorUDP.java
+++ src/ServidorUDP.java
@@ -1,2 +1,25 @@
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.SocketException;
+
 public class ServidorUDP {
+    public static void main (String[] args){
+        try  {
+            DatagramSocket udpSocket = new DatagramSocket(9107);
+            byte[] buffer = new byte [1024];
+            while (true){
+                DatagramPacket dPacket = new DatagramPacket(buffer, buffer.length);
+                    udpSocket.receive(dPacket);
+                    System.out.print("ip: " + dPacket.getAddress());
+                    System.out.print("puerto: " + dPacket.getPort());
+                    System.out.print("mensaje: " + new String(dPacket.getData(), 0 , dPacket.getLength()));
+
+            }
+        } catch (SocketException e){
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
